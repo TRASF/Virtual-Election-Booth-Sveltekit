c9f2f58cb9242dbec6d6031dc3d7ece91735b001
TRASF <thantamrong.jir@student.mahidol.ac.th>
2023-04-02 14:55:58 +0700

    Fourth version - Modified to prevent repeated voting.

--- src/routes/vote/+page.svelte
+++ src/routes/vote/+page.svelte
@@ -1,129 +1,148 @@
 <script>
-  import { auth } from '$lib/firebaseConfig';
-  import { collection, getDoc, orderBy, limit, query, getDocs, doc, setDoc } from 'firebase/firestore';
-  import { firestore } from '$lib/firebaseConfig';
-  import { onMount } from 'svelte';
-  import { getCandidates } from '$lib/getCandidates';
-  import { incrementCandidateVote, setUserHasVoted } from '$lib/vote';
+	import { auth } from '$lib/firebaseConfig';
+	import {
+		collection,
+		getDoc,
+		orderBy,
+		limit,
+		query,
+		getDocs,
+		doc,
+		setDoc,
+		where
+	} from 'firebase/firestore';
+	import { firestore } from '$lib/firebaseConfig';
+	import { onMount } from 'svelte';
+	import { getCandidates } from '$lib/getCandidates';
+	import { incrementCandidateVote, setUserHasVoted } from '$lib/vote';
 
-  /**
+	/**
 	 * @type {any[]}
 	 */
-  let candidates = [];
-  let selectedCandidate = '';
+	let candidates = [];
+	let selectedCandidate = '';
 
-
-  /**
+	/**
 	 * @param {any} _uid
 	 * @param {string} _selectedCandidate
 	 */
-  async function submitVote(_uid, _selectedCandidate) {
-    if (!selectedCandidate) return;
+	async function submitVote(_uid, _selectedCandidate) {
+		if (!selectedCandidate) return;
 
-    const votesQuery = query(
-    collection(firestore, 'votes'),
-    orderBy('timestamp', 'desc'),
-    limit(1)
-  );
-  
-  const votesSnapshot = await getDocs(votesQuery);
-  const latestVote = votesSnapshot.docs[0];
-    const vote = {
-      userId: _uid,
-      votedFor: _selectedCandidate,
-      timestamp: new Date().toISOString(),
-      prevVoteId: latestVote ? latestVote.id : null,
-    };
-    
-    const voteRef = doc(firestore, 'votes', `${_uid}_${_selectedCandidate}`);
+		const votesRef = collection(firestore, 'votes');
+		const userVoteQuery = query(
+			votesRef,
+			where('userId', '==', _uid),
+			where('votedFor', '==', _selectedCandidate)
+		);
+		const userVoteSnapshot = await getDocs(userVoteQuery);
 
-    try {
-      await setDoc(voteRef, vote);
-      await incrementCandidateVote(selectedCandidate);
-      await setUserHasVoted(_uid, _selectedCandidate);
-      verifyVoteChain();
-      alert('Vote submitted successfully!');
-      return true;
-    } catch (error) {
-      console.error('Error submitting vote:', error);
-      alert('An error occurred while submitting your vote. Please try again.');
-      return false;
-    }
-    
-  }
+		if (!userVoteSnapshot.empty) {
+			alert('You have already voted for this candidate.');
+			return;
+		}
 
-  async function verifyVoteChain() {
-  let currentVoteId = null;
-  let valid = true;
+		const votesQuery = query(
+			collection(firestore, 'votes'),
+			orderBy('timestamp', 'desc'),
+			limit(1)
+		);
 
-  while (valid) {
-    // @ts-ignore
-    const currentVoteRef = doc(firestore, 'votes', currentVoteId);
-    // @ts-ignore
-    const currentVoteDoc = await getDoc(currentVoteRef);
+		const votesSnapshot = await getDocs(votesQuery);
+		const latestVote = votesSnapshot.docs[0];
+		const vote = {
+			userId: _uid,
+			votedFor: _selectedCandidate,
+			timestamp: new Date().toISOString(),
+			prevVoteId: latestVote ? latestVote.id : null
+		};
 
-    if (!currentVoteDoc.exists()) {
-      break;
-    }
+		const voteRef = doc(firestore, 'votes', `${_uid}_${_selectedCandidate}`);
 
-    // @ts-ignore
-    const currentVoteData = currentVoteDoc.data();
-    if (currentVoteData.prevVoteId !== currentVoteId) {
-      valid = false;
-      console.error('Invalid vote chain at:', currentVoteId);
-      break;
-    }
+		try {
+			await setDoc(voteRef, vote);
+			await incrementCandidateVote(selectedCandidate);
+			await setUserHasVoted(_uid, _selectedCandidate);
+			verifyVoteChain();
+			alert('Vote submitted successfully!');
+		} catch (error) {
+			console.error('Error submitting vote:', error);
+			alert('An error occurred while submitting your vote. Please try again.');
+		}
+	}
 
-    currentVoteId = currentVoteData.prevVoteId;
-  }
+	async function verifyVoteChain() {
+		let currentVoteId = null;
+		let valid = true;
 
-  if (valid) {
-    console.log('Vote chain verified');
-  }
-}
+		while (valid) {
+			// @ts-ignore
+			const currentVoteRef = doc(firestore, 'votes', currentVoteId);
+			// @ts-ignore
+			const currentVoteDoc = await getDoc(currentVoteRef);
 
-  onMount(async () => {
-    candidates = await getCandidates();
-  });
+			if (!currentVoteDoc.exists()) {
+				break;
+			}
 
-  async function handleVoteSubmit() {
-    if (!selectedCandidate) {
-      console.error('No candidate selected');
-      return;
-    }
+			// @ts-ignore
+			const currentVoteData = currentVoteDoc.data();
+			if (currentVoteData.prevVoteId !== currentVoteId) {
+				valid = false;
+				console.error('Invalid vote chain at:', currentVoteId);
+				break;
+			}
 
-    const  uid  = auth.currentUser?.uid;
-    if (!uid) {
-      console.error('User ID not found');
-      return;
-    }
+			currentVoteId = currentVoteData.prevVoteId;
+		}
 
-    try {
-      await submitVote(uid, selectedCandidate);
-      console.log('Vote submitted successfully');
-      // Redirect or show a success message
-    } catch (error) {
-      console.error('Error submitting vote:', error);
-    }
-  }
+		if (valid) {
+			console.log('Vote chain verified');
+		}
+	}
 
+	onMount(async () => {
+		candidates = await getCandidates();
+	});
+
+	async function handleVoteSubmit() {
+		if (!selectedCandidate) {
+			console.error('No candidate selected');
+			return;
+		}
+
+		const uid = auth.currentUser?.uid;
+		if (!uid) {
+			console.error('User ID not found');
+			return;
+		}
+
+		try {
+			await submitVote(uid, selectedCandidate);
+			console.log('Vote submitted successfully');
+			// Redirect or show a success message
+		} catch (error) {
+			console.error('Error submitting vote:', error);
+		}
+	}
 </script>
+
 <h1>Vote for a Candidate</h1>
 <form on:submit|preventDefault={handleVoteSubmit}>
-  <fieldset>
-    <legend>Select a candidate:</legend>
-    {#each candidates as candidate (candidate.id)}
-      <div>
-        <input
-          type="radio"
-          id="candidate-{candidate.id}"
-          name="candidate"
-          value="{candidate.id}"
-          bind:group={selectedCandidate}
-        />
-        <label for="candidate-{candidate.id}">{candidate.name}</label>
-      </div>
-    {/each}
-  </fieldset>
-  <button type="submit">Submit Vote</button>
+	<fieldset>
+		<legend>Select a candidate:</legend>
+		{#each candidates as candidate (candidate.id)}
+			<div>
+				<input
+					type="radio"
+					id="candidate-{candidate.id}"
+					name="candidate"
+					value={candidate.id}
+					bind:group={selectedCandidate}
+				/>
+				<label for="candidate-{candidate.id}">{candidate.name}</label>
+			</div>
+		{/each}
+	</fieldset>
+	<button type="submit">Submit Vote</button>
 </form>
